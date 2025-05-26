fdd85756c9254ea384b61a0d776f1d0a439f9992
front-lucy <devfront@jaedam.com>
2025-05-26 14:00:04 +0900

    토큰 모듈 개선: border, radius, shadow를 개별적으로 가져오고 타입 정의 추가

--- src/tokens/index.ts
+++ src/tokens/index.ts
@@ -1,12 +1,13 @@
-export * as border from "./border";
-export * as radius from "./radius";
-export * as shadow from "./shadow";
-export { colors };
+import * as border from "./border";
 import * as colors from "./colors";
-export type ColorType = keyof typeof colors;
-
+import * as radius from "./radius";
+import * as shadow from "./shadow";
 import { typography } from "./typography";
 
-export { typography };
+export { border, colors, radius, shadow, typography };
 
-export type TypographyType = keyof typeof typography;
+export type ColorToken = keyof typeof colors;
+export type TypographyToken = keyof typeof typography;
+export type BorderToken = keyof typeof border;
+export type RadiusToken = keyof typeof radius;
+export type ShadowToken = keyof typeof shadow;
