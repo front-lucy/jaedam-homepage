729dded22f106b15f0bda49fd3096bcb61cf4f97
front-lucy <devfront@jaedam.com>
2025-05-27 13:34:48 +0900

    버튼 컴포넌트 삭제: 페이지에서 버튼을 제거하여 레이아웃 간소화

---
+++ src/app/business/page.tsx
@@ -0,0 +1,7 @@
+export default function BusinessPage() {
+  return (
+    <main>
+      <h1>Business</h1>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Button } from "@/components/atom/button";
 import { Footer } from "@/components/molecules/footer";
 import { Header } from "@/components/molecules/header";
 
@@ -10,9 +9,6 @@ export default function Home() {
       <Header pageType="sub" mode="dark" />
 
       <div style={{ width: "100%", height: "150vh" }}>나는야 버튼 입니다.</div>
-      <Button variant="tertiary" size="large" state="default">
-        나는야 버튼 입니다.
-      </Button>
 
       <Footer />
     </div>
