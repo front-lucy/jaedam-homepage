c2a88286f1bea74483f512f917296961df4bb9cf
정성훈 <shjung@surromind.ai>
2025-06-01 15:00:18 +0900

    .idea 디렉토리에 프로젝트 설정 파일 추가: .gitignore, 코드 스타일, ESLint 설정 및 모듈 정보 포함. 기존 컴포넌트 인덱스 파일 삭제.

--- src/app/_components/index.ts
+++
@@ -1,5 +0,0 @@
-export { HeroSection } from "./hero-section";
-export { BestSection } from "./best-section";
-export { ServicesSection } from "./services-section";
-export { AboutSection } from "./about-section";
-export { ContactSection } from "./contact-section";
---
+++ src/app/components/index.ts
@@ -0,0 +1,6 @@
+export { AboutSection } from './about-section';
+export { BestSection } from './best-section';
+export { ContactSection } from './contact-section';
+export { HeroSection } from './hero-section';
+export { ServicesSection } from './services-section';
+
