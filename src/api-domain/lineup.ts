133a15f4e69d12430cf8b17d53310bc16fddbff9
정성훈 <shjung@surromind.ai>
2025-06-04 10:19:35 +0900

    chore: mock 해제

--- src/api-domain/lineup.ts
+++ src/api-domain/lineup.ts
@@ -1,17 +1,17 @@
 // v1/jaedam/homepage/lineup
 
-// import { globalCommonApi } from "@/shared/api/commonApi";
+import { globalCommonApi } from "@/shared/api/commonApi";
 import { ApiWrapped } from "@/types/api";
 import { LineUpResponse } from "@/types/lineup";
-import mockLineupResponse from "../../mock-lineup.json";
+// import mockLineupResponse from "../../mock-lineup.json";
 
 export async function getLineup (): Promise<ApiWrapped<LineUpResponse>> {
-  // return await globalCommonApi<ApiWrapped<LineUpResponse>>({
-  //   url: `lineup`,
-  //   method: "GET",
-  // })
-
-  return new Promise((resolve) => {
-    resolve(mockLineupResponse);
+  return await globalCommonApi<ApiWrapped<LineUpResponse>>({
+    url: `lineup`,
+    method: "GET",
   })
+
+  // return new Promise((resolve) => {
+  //   resolve(mockLineupResponse);
+  // })
 }
